45ec374e520783bf5b3c9da4ddf0f0c6755ee05b
Jeffrey Wan <Jwan622@gmail.com>
2015-05-07 15:52:07 -0600

    added some comments with jeffrey

--- app/router.js
+++ app/router.js
@@ -6,4 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
+  this.resource('artists', function() {
+    this.resource("artist", { path: ":artist_id" }, function() {
+      // here we are overridng the url with the path id... that's why the url reads /artists/1 This artist id shows up in the params
+      this.resource('albums', function() {
+        this.resource('album', { path: ":album_id" })
+      });
+    });
+  });
 });
---
+++ app/routes/artist.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+});
+// name of the route and the id that is passed in. That's what mode lis in teh artist.hbs by default so naming is super important of the route
